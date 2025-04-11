3c2de27fc2b85e5488a8ce5a79faa4d4bb08254f
GesJeremie <ges.jeremie@gmail.com>
2025-04-11 18:08:37 -0500

    Add average resolved time

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,6 +1,14 @@
 class IncidentsController < ApplicationController
   def index
-    #Incident.create(kind: 'pending_reply', source_details: { platform: 'airbnb', conversation_id: '38383' })
+    analytics_resolved_by = params[:analytics_resolved_by]
+
+    time_scope = Incident.where.not(resolved_at: nil)
+    time_scope = time_scope.where(resolved_by: analytics_resolved_by) if analytics_resolved_by.present?
+
+    @average_resolved_time = time_scope.average("EXTRACT(EPOCH FROM resolved_at - created_at)")
+
+    @resolved_by_options = Incident.where.not(resolved_by: nil).distinct.pluck(:resolved_by)
+
     @incidents = IncidentViewModel.wrap(Incident.order(created_at: :desc).all)
   end
 
