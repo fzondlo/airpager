9f803252c89ac834060bf4640294423128d174be
GesJeremie <ges.jeremie@gmail.com>
2025-04-19 15:07:07 -0500

    Slightly refactor process logic hospitable_webhooks_controller

--- app/controllers/hospitable_webhooks_controller/message_created_handler.rb
+++ app/controllers/hospitable_webhooks_controller/message_created_handler.rb
@@ -9,37 +9,18 @@ class HospitableWebhooksController
     def perform
       store_message
 
-      if message.from_guest?
-        handle_guest_message
-      elsif message.from_team?
-        handle_team_message
+      if message.from_team?
+        return resolve_pending_incident
+      end
+
+      if message.from_guest? && !pending_incident.present?
+        return create_incident
+
+        # NotifyTeamOfIncidentWorker.perform_in(15.minutes, incident_id: incident.id)
       end
     end
 
     private
-
-    def handle_guest_message
-      return if pending_incident.present?
-
-      # TODO: Generate prompt for ChatGPT with a class
-      # TODO: Send prompt to ChatGPT to check if needs reply from team
-      # TODO: If not, don't create incident
-
-      Incident.create!(
-        kind: 'pending_reply',
-        source_details: {
-          platform: message.platform,
-          conversation_id: message.conversation_id,
-          reservation_id: message.reservation_id
-        }
-      )
-
-      # NotifyTeamOfIncidentWorker.perform_in(15.minutes, incident_id: incident.id)
-    end
-
-    def handle_team_message
-      pending_incident&.resolve!(by: message.sender_full_name)
-    end
 
     def store_message
       Message.create!(
@@ -50,6 +31,21 @@ class HospitableWebhooksController
         content: message.body,
         posted_at: message.created_at
       )
+    end
+
+    def create_incident
+      Incident.create!(
+        kind: 'pending_reply',
+        source_details: {
+          platform: message.platform,
+          conversation_id: message.conversation_id,
+          reservation_id: message.reservation_id
+        }
+      )
+    end
+
+    def resolve_pending_incident
+      pending_incident&.resolve!(by: message.sender_full_name)
     end
 
     def pending_incident
