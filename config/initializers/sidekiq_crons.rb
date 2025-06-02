7b586f78f2281c5fe936f0b765ef411cf1c47fbc
Frank C Zondlo <fzondlo@Franks-MacBook-Pro-2.local>
2025-06-02 11:39:32 -0500

    updated cron schedule

--- config/initializers/sidekiq_crons.rb
+++ config/initializers/sidekiq_crons.rb
@@ -1,12 +1,12 @@
-EVERY_DAY   = "0 0 * * *"
+EVERY_HOUR = "0 * * * *"
 AT_8_30_AM  = "30 8 * * *"
 AT_11_AM    = "0 11 * * *"
 TIME_ZONE   = Time.zone.tzinfo.identifier
 
 Sidekiq::Cron::Job.create(
-  name:  'SyncClickupReservationsWorker – midnight',
+  name:  'SyncClickupReservationsWorker – EVERY_HOUR',
   klass: 'SyncClickupReservationsWorker',
-  cron:  "#{EVERY_DAY} #{TIME_ZONE}"
+  cron:  "#{EVERY_HOUR} #{TIME_ZONE}"
 )
 
 # Run at 8:30 AM every day
