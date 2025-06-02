80cea4dfbbbd004c9369911817c9666bd93fa9bb
Frank C Zondlo <fzondlo@mac.lan>
2025-06-02 12:59:58 -0500

    trying to see if we need 2 minutes to sync airbnb rerservations

--- app/controllers/hospitable_webhooks_controller/reservation_updated_handler.rb
+++ app/controllers/hospitable_webhooks_controller/reservation_updated_handler.rb
@@ -6,6 +6,7 @@ class HospitableWebhooksController
 
     def perform
       SyncClickupReservationsWorker.perform_async
+      SyncClickupReservationsWorker.perform_in(2.minutes)
     end
   end
 end
