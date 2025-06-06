dee6573a9c4a304497bc3252f1a39a2aeaedf726
Frank C Zondlo <fzondlo@Franks-MacBook-Pro-2.local>
2025-06-06 13:41:30 -0500

    fix task date issue

--- app/models/task.rb
+++ app/models/task.rb
@@ -37,8 +37,8 @@ class Task
     tomorrow_start = Time.new(tomorrow.year, tomorrow.month, tomorrow.day, 0, 0, 0)
     tomorrow_end = Time.new(tomorrow.year, tomorrow.month, tomorrow.day, 23, 59, 59)
     params = {
-      due_date_gt: tomorrow_start.to_i * 1000,
-      due_date_lt: tomorrow_end.to_i * 1000,
+      start_date_gt: tomorrow_start.to_i * 1000,
+      start_date_lt: tomorrow_end.to_i * 1000,
       order_by: "start_date",
       statuses: [ "ACTIVO" ],
       reverse: true
@@ -51,8 +51,8 @@ class Task
     today_start = Time.new(today.year, today.month, today.day, 0, 0, 0)
     today_end = Time.new(today.year, today.month, today.day, 23, 59, 59)
     params = {
-      due_date_gt: today_start.to_i * 1000,
-      due_date_lt: today_end.to_i * 1000,
+      start_date_gt: today_start.to_i * 1000,
+      start_date_lt: today_end.to_i * 1000,
       order_by: "start_date",
       statuses: [ "ACTIVO" ],
       reverse: true
@@ -71,7 +71,7 @@ class Task
     ].to_json
     params = {
       custom_fields: custom_fields_filter,
-      due_date_gt: now_in_ms,
+      start_date_gt: now_in_ms,
       order_by: "start_date",
       statuses: [ "ACTIVO" ],
       reverse: true
