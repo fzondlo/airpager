6d0727d19d99447e6168b8a0352fa65b48160c0f
GesJeremie <ges.jeremie@gmail.com>
2025-07-07 07:10:29 -0500

    Improve filters conversation_messages

--- app/view_models/incident_view_model.rb
+++ app/view_models/incident_view_model.rb
@@ -80,9 +80,9 @@ class PendingReplyIncidentKind < BaseIncidentKind
 
   def conversation_messages
     @conversation_messages ||= begin
-      scope = Message.where(conversation_id: conversation_id, reservation_id: reservation_id)
-                     .where("posted_at >= ?", model.created_at)
-
+      scope = Message.where(conversation_id: conversation_id)
+      scope = scope.where(reservation_id: reservation_id) if reservation_id.present?
+      scope = scope.where("posted_at >= ?", model.created_at)
       scope = scope.where("posted_at <= ?", model.resolved_at) if model.resolved_at.present?
 
       MessageViewModel.wrap(scope.order(posted_at: :asc).all)
