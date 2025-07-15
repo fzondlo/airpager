a9e43a5c1981e1070987d76db265e51f6076dc16
GesJeremie <ges.jeremie@gmail.com>
2025-07-15 10:45:01 -0500

    Start testing auto_reply_identifier

--- app/controllers/hospitable_webhooks_controller/suggested_reply.rb
+++ app/controllers/hospitable_webhooks_controller/suggested_reply.rb
@@ -1,41 +1,43 @@
-class SuggestedReply
-  include Task::Mapping
+class HospitableWebhooksController
+  class SuggestedReply
+    include Task::Mapping
 
-  attr_reader :message
+    attr_reader :message
 
-  def initialize(message)
-    @message = message
-  end
+    def initialize(message)
+      @message = message
+    end
 
-  def log
-    body =
-      <<~TEXT
-        New Message from #{message.sender_full_name}:
-        #{message.content}
+    def log
+      body =
+        <<~TEXT
+          New Message from #{message.sender_full_name}:
+          #{message.content}
 
-        -----
+          -----
 
-        Suggested Reply:
-        #{auto_reply&.reply || "Sorry, I don't have an answer for that."}
-      TEXT
+          Suggested Reply:
+          #{auto_reply&.reply || "Sorry, I don't have an answer for that."}
+        TEXT
 
-    Waapi.gateway.send_message(body, LOGGING_WA_GROUP)
+      Waapi.gateway.send_message(body, LOGGING_WA_GROUP)
 
-    # TODO: Track usage locally in a database table (conversation_id / auto_reply_id)
-  end
+      # TODO: Track usage locally in a database table (conversation_id / auto_reply_id)
+    end
 
-  private
+    private
 
-  def property_id
-    @property_id ||= PropertyIdentifier.new(message).resolve
-  end
+    def property_id
+      @property_id ||= PropertyIdentifier.new(message).resolve
+    end
 
-  def auto_reply
-    return unless property_id.present?
+    def auto_reply
+      return unless property_id.present?
 
-    @auto_reply ||= AutoReplyIdentifier.new(
-      message: message.content,
-      property_id: property_id
-    ).resolve
+      @auto_reply ||= ::AutoReplyIdentifier.new(
+        message: message.content,
+        property_id: property_id
+      ).resolve
+    end
   end
 end
