d634ca7a4d9ed05eb93bd4b82de64a47a87941d8
GesJeremie <ges.jeremie@gmail.com>
2025-07-17 07:21:01 -0500

    Run rubocop linter

--- app/models/auto_reply.rb
+++ app/models/auto_reply.rb
@@ -5,7 +5,7 @@ class AutoReply < ApplicationRecord
   validates :trigger, presence: true
   validates :reply, presence: true
 
-  validates :live_enabled, inclusion: { in: [true, false] }
+  validates :live_enabled, inclusion: { in: [ true, false ] }
 
   scope :live_enabled, -> { where(live_enabled: true) }
 end
