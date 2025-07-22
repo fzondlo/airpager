8ff217ae1f4593b355363dfb1669110185598bbf
GesJeremie <ges.jeremie@gmail.com>
2025-07-22 10:47:58 -0500

    Guard AutoReplyUsageViewModel#message_trigger_content

--- app/view_models/auto_reply_usage_view_model.rb
+++ app/view_models/auto_reply_usage_view_model.rb
@@ -1,5 +1,7 @@
 class AutoReplyUsageViewModel < ApplicationViewModel
   def message_trigger_content
+    return unless model.message_trigger_id.present?
+
     @message_trigger_content ||= Message.find(model.message_trigger_id)&.content
   end
 end
